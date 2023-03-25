f2029c3ceb8043b258d19ef6113e6efcdabae49b
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-24 22:12:31 -0400

    added 8th exercise

--- loops2.rb
+++ loops2.rb
@@ -60,15 +60,22 @@
 
 # 7. Write a while loop that prints the numbers 50 to 70.
 
-x = 50
+# x = 50
 
-while x < 71
-  p x
-  x += 1
-end
+# while x < 71
+#   p x
+#   x += 1
+# end
 
 
 # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+i = 1
+
+while i < 145
+  p "Around the world"
+  i += 1
+end
 
 # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
