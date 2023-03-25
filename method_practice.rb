88105c43b2dcf8f964a266debce1f86c9269d43e
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-24 21:15:59 -0400

    added 2nd exercise

--- method_practice.rb
+++ method_practice.rb
@@ -12,11 +12,11 @@
 
 # number = gets.chomp.to_i
 
-# if number > 100
-#   p "That's a big number"
-# else
-#   p "Thank you"
-# end
+if number > 100
+  p "That's a big number"
+else
+  p "Thank you"
+end
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
