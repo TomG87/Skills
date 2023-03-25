b87089431a74b2ab8ab5b4dd25d886da23963ac4
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-24 22:03:35 -0400

    added 4th exercise

--- loops2.rb
+++ loops2.rb
@@ -19,16 +19,23 @@
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
-p "Please enter a word.  When you want the program to stop running please enter the word 'stop'"
+# p "Please enter a word.  When you want the program to stop running please enter the word 'stop'"
 
-while  true
-  word = gets.chomp 
-  if word == "stop"
-    break
-  end
-end
+# while  true
+#   word = gets.chomp 
+#   if word == "stop"
+#     break
+#   end
+# end
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+i = 0
+
+while i < 101
+  p i
+  i += 5
+end
 
 # 5. Write a while loop that prints the number 9000 ten times.
 
