1af3b97d4349142d77b92208dfb796c247085288
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-26 16:24:58 -0400

    added 5h exercise

--- array_map.rb
+++ array_map.rb
@@ -38,17 +38,26 @@
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
 
-array = [1, 2, 3]
-new_array = []
+# array = [1, 2, 3]
+# new_array = []
 
-array.each do |arr|
-  new_array << arr + 7
-end
+# array.each do |arr|
+#   new_array << arr + 7
+# end
 
-p new_array
+# p new_array
 
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
+
+array = ["mississippi", "antarctica", "molasses"]
+new_array = []
+
+array.each do |arr|
+  new_array << arr.length
+end
+
+p new_array
 
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
