cb1464276743db0de5b31acf1b65db65c81fcd86
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-26 15:48:07 -0400

    added 2nd exercise

--- functions_practice.rb
+++ functions_practice.rb
@@ -8,6 +8,12 @@
 
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
 
+def string(word)
+  return word.upcase
+end
+
+p string("Hall of Fame")
+
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
 # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
