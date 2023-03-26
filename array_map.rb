9c6f9f3fcdb6725d5b4f96e63fa138dc798896e0
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-26 16:23:08 -0400

    added 4th exercise

--- array_map.rb
+++ array_map.rb
@@ -25,18 +25,27 @@
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
 
-people = [{name: "Alice", age: 27}, {name: "Blane", age: 16}] 
-array = []
+# people = [{name: "Alice", age: 27}, {name: "Blane", age: 16}] 
+# array = []
 
-people.each do |person|
-  array << person[:name]
-end
+# people.each do |person|
+#   array << person[:name]
+# end
 
-p array
+# p array
 
 
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
+
+array = [1, 2, 3]
+new_array = []
+
+array.each do |arr|
+  new_array << arr + 7
+end
+
+p new_array
 
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
