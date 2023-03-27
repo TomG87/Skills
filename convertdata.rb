4fa33280c4c2e8e01cd2bcf87cb64c545eb230dd
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-26 20:37:09 -0400

    added 2nd exercise

--- convertdata.rb
+++ convertdata.rb
@@ -1,18 +1,29 @@
 #  1. Start with an array of numbers and compute the sum of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 26.
 
-array = [5, 10, 8, 3]
+# array = [5, 10, 8, 3]
 
 
-sum = 0
-array.each do |arr|
-  sum += arr
-end
+# sum = 0
+# array.each do |arr|
+#   sum += arr
+# end
 
-p sum
+# p sum
 
 #  2. Start with an array of strings and combine them all into a single string.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+array = ["volleyball", "basketball", "badminton"]
+
+total = ""
+
+
+array.each do |arr|
+  total += arr
+end
+
+p total
 
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
