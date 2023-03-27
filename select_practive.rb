5b38b3443f200687ebf1e15caad0b4224c911da4
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-26 20:12:17 -0400

    added 5th exercise

--- select_practive.rb
+++ select_practive.rb
@@ -51,19 +51,30 @@
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
-array = [2, 4, 5, 1, 8, 9, 7] 
+# array = [2, 4, 5, 1, 8, 9, 7] 
+# new_array = []
+
+# array.each do |arr|
+#   if arr.even? == true
+#   new_array << arr
+#   end
+# end
+
+# p new_array
+
+#  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
+#     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+array = ["a", "man", "a", "plan", "a", "canal", "panama"]
 new_array = []
 
 array.each do |arr|
-  if arr.even? == true
-  new_array << arr
+  if arr.length < 4
+    new_array << arr
   end
 end
 
 p new_array
-
-#  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
-#     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
